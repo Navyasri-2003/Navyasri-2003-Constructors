cd5c72f68824bac5af3259b5eca40b55aad1785c
Navyasri-2003 <navyasriap27aq8346@gmail.com>
2025-08-29 22:45:08 +0530

    con

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -31,7 +31,8 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
-
+        this.playerName=playerName;
+        this.level=1;
     }
 
     /**
@@ -48,7 +49,15 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
-
+        this.playerName=playerName;
+        this.level=level;
     }
 
+    public static void main(String[] args) {
+        Player newPlayer1 = new Player("Mario");
+        Player newPlayer2 = new Player("Mario", 35);
+
+        System.out.println(newPlayer1.playerName + " - Level " + newPlayer1.level);
+        System.out.println(newPlayer2.playerName + " - Level " + newPlayer2.level);
+    }
 }
